c82ad7834470b1861ed69f6d45e3a51cd706679d
Eneko <info@enekogarrido.com>
2024-02-22 13:07:26 +0100

    Update wp-carbonbadge.php

--- wp-carbonbadge.php
+++ wp-carbonbadge.php
@@ -1,13 +1,13 @@
 <?php
 /**
  * Plugin Name:       WP Carbonbadge
- * Description:       Website Carbon Badge for WordPress, created with the WordPress Interactivity API.
+ * Description:       The Website Carbon widget Gutenberg block, made with the Interactivity API.
  * Version:           0.1.0
  * Requires at least: 6.5
  * Requires PHP:      7.0
  * Author:            garridinsi
  * License:           GPL-3.0-or-later
- * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
  * Text Domain:       wp-carbonbadge
  *
  * @package           wp-carbonbadge
